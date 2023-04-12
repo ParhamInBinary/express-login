51310a974b9e221584a721837dd7ff426ca23021
ParhamInBinart <parham.berenjian@gmail.com>
2023-04-12 11:44:19 +0200

    encrypt pw with argon2

--- server.ts
+++ server.ts
@@ -1,45 +1,51 @@
-import cookieSession from 'cookie-session';
-import express from 'express';
-import fs from 'fs';
+import argon2 from "argon2";
+import cookieSession from "cookie-session";
+import express from "express";
 
 // SERVER APPLICATION
 const app = express();
 
 // DATABASE
-interface User  {
-    email: string;
-    password: string;
+interface User {
+  email: string;
+  password: string;
 }
 
-const users: User[] = []
+const users: User[] = [];
 
 // GLOBAL MIDDLEWARES
 // 1. json
 // 2. cookie-session
 app.use(express.json());
-app.use(cookieSession({
-    name: 'login',
+app.use(
+  cookieSession({
+    name: "login",
     secure: false,
     httpOnly: true,
-    secret: 'jqgi326kdf76fs8d9f6s9fsafs135ts',
+    secret: "jqgi326kdf76fs8d9f6s9fsafs135ts",
     maxAge: 1000 * 20,
-}));
+  })
+);
 
-app.get('/', (req, res) => {
-    res.status(200).json("Welcome to my API, please login!")
+app.get("/", (req, res) => {
+  res.status(200).json("Welcome to my API, please login!");
 });
 
-app.get('/users', (req, res) => {
-    res.status(200).json(users)
-})
+app.get("/users", (req, res) => {
+  res.status(200).json(users);
+});
 
-app.post('/users/register', (req, res) => {
-    const { email, password } = req.body;
-    const user = { email, password };
-    users.push(user);
+app.post("/users/register", async (req, res) => {
+  const { email, password } = req.body;
+  const hashedPW = await argon2.hash(password);
 
-    res.status(201).json(user)
-})
+  const user = { email, password: hashedPW };
+  users.push(user);
+
+  res.status(201).json(user);
+});
 
 // START THE SERVER
-app.listen(3000, () => console.log('Running on: http://localhost:3000'));
+app.listen(3000, () =>
+  console.log("Running on: http://localhost:3000")
+);

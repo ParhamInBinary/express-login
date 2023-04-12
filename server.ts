d81a231ac7792e51add7c65c2de04dfb4cacafa9
ParhamInBinart <parham.berenjian@gmail.com>
2023-04-12 11:13:09 +0200

    add express start code + rest file

--- server.ts
+++ server.ts
@@ -4,6 +4,10 @@ import fs from 'fs';
 const app = express();
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.status(200).json("Welcome to my API, please login!")
+});
+
 
 
 app.listen(3000, () => console.log('Running on: http://localhost:3000'));

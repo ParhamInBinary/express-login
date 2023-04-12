d653c3563f93db1513d3ec48388aac2c142daff7
ParhamInBinart <parham.berenjian@gmail.com>
2023-04-12 13:30:59 +0200

    refactor logic into controller file

---
+++ controller.ts
@@ -0,0 +1,67 @@
+import argon2 from "argon2";
+import { Request, Response } from "express";
+
+// DATABASE
+interface User {
+  email: string;
+  password: string;
+}
+
+const users: User[] = [];
+
+export function getAllUsers(req: Request, res: Response) {
+  res.status(200).json(users);
+}
+
+export async function registerUser(
+  req: Request,
+  res: Response
+) {
+  const { email, password } = req.body;
+  const hashedPW = await argon2.hash(password);
+
+  const user = { email, password: hashedPW };
+  users.push(user);
+
+  res.status(201).json(user);
+}
+
+export async function loginUser(
+  req: Request,
+  res: Response
+) {
+  const { email, password } = req.body;
+
+  // CHECK USER
+  const user = users.find((u) => u.email === email);
+  if (!user) {
+    return res
+      .status(400)
+      .json("Incorrect email or password");
+  }
+
+  // CHECK PASSWORD
+  const isAuth = await argon2.verify(
+    user.password,
+    password
+  );
+  if (!isAuth) {
+    return res
+      .status(400)
+      .json("Incorrect email or password");
+  }
+
+  // CREATE SESSION/COOKIE
+  req.session!.email = user.email;
+
+  // SEND RESPONS
+  res.status(200).json("Login successful!");
+}
+
+export function getAuth(req: Request, res: Response) {
+  if (!req.session?.email) {
+    return res.status(401).json("You must login!");
+  }
+
+  res.status(200).json(req.session);
+}
--- server.ts
+++ server.ts
@@ -1,17 +1,14 @@
-import argon2 from "argon2";
 import cookieSession from "cookie-session";
 import express from "express";
+import {
+  getAllUsers,
+  getAuth,
+  loginUser,
+  registerUser,
+} from "./controller";
 
 // SERVER APPLICATION
 const app = express();
-
-// DATABASE
-interface User {
-  email: string;
-  password: string;
-}
-
-const users: User[] = [];
 
 // GLOBAL MIDDLEWARES
 // 1. json
@@ -31,50 +28,13 @@ app.get("/", (req, res) => {
   res.status(200).json("Welcome to my API, please login!");
 });
 
-app.get("/users", (req, res) => {
-  res.status(200).json(users);
-});
+app.get("/users", getAllUsers);
 
-app.post("/users/register", async (req, res) => {
-  const { email, password } = req.body;
-  const hashedPW = await argon2.hash(password);
+app.post("/users/register", registerUser);
 
-  const user = { email, password: hashedPW };
-  users.push(user);
+app.post("/users/login", loginUser);
 
-  res.status(201).json(user);
-});
-
-app.post('/users/login', async(req, res) => {
-    const { email, password } = req.body;
-    
-    // CHECK USER
-    const user = users.find( u => u.email === email);
-    if (!user) {
-        return res.status(400).json('Incorrect email or password')
-    }
-    
-    // CHECK PASSWORD
-    const isAuth = await argon2.verify(user.password, password);
-    if (!isAuth) {
-        return res.status(400).json('Incorrect email or password')
-    }
-
-    // CREATE SESSION/COOKIE
-    req.session!.email = user.email;
-
-    // SEND RESPONS
-    res.status(200).json('Login successful!')
-})
-
-
-app.get('/users/auth', (req, res) => {
-    if (!req.session?.email) {
-        return res.status(401).json('You must login!');
-    }
-
-    res.status(200).json(req.session);
-})
+app.get("/users/auth", getAuth);
 
 // START THE SERVER
 app.listen(3000, () =>
